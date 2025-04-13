f3c413b62061dbe1386d6ad32f68f6187c2ef4a4
Adityanpx <137485127+Adityanpx@users.noreply.github.com>
2025-04-13 12:25:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+document.getElementById("submit").addEventListener("click", function (){
+	const title= document.getElementById("title").value;
+	const author= document.getElementById("author").value;
+	const isbn = document.getElementById("isbn").value;
+
+	if(!title || !author || !isbn){
+		alert("please fill all the details");
+		return;
+	}
+
+	const table = document.getElementById("book-list");
+
+	const row = document.createElement("tr");
+	row.innerHTML= `<td>${title}</td>
+	<td>${author}</td>
+	<td>${isbn}</td>
+	<td><button class="delete">X</button></td>`
+
+	table.appendChild(row)
+	document.getElementById("title").value = '';
+	document.getElementById("author").value = '';
+
+	document.getElementById("isbn").value = '';
+})
+
+document.getElementById("book-list").addEventListener("click",function(event){
+	if(event.target.classList.contains("delete")){
+		event.target.closest("tr").remove();
+	}
+})
